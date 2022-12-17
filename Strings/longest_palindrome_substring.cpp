58c6c55d1200d0e3742ca830230b8e7126249467
divya034 <59786992+divya034@users.noreply.github.com>
2022-12-17 21:16:07 +0530

    Create longest_palindrome_substring.cpp

---
+++ Strings/longest_palindrome_substring.cpp
@@ -0,0 +1,2 @@
+https://leetcode.com/problems/longest-palindromic-substring/solutions/2063953/c/
+https://leetcode.com/problems/longest-palindromic-substring/solutions/2671545/c-easy-fast-and-short-code/
