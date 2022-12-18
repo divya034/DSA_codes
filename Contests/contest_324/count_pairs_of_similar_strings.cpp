f2657d3b0728c78864822f18f20abcd5a5b8f43e
divya034 <59786992+divya034@users.noreply.github.com>
2022-12-18 14:20:09 +0530

    Update count_pairs_of_similar_strings.cpp

--- Contests/contest_324/count_pairs_of_similar_strings.cpp
+++ Contests/contest_324/count_pairs_of_similar_strings.cpp
@@ -38,3 +38,15 @@ Stdout
 ab 2
 abc 2
 abcd 1
+    
+   unordered_map<set<char> , int> mp; THIS WONT WORK ERROR CANT PASS SET IN UNORDERED MAP. CHECK GEEKFOR GEEKS
+Unordered Map does not contain a hash function for a pair like it has for int, string, etc,
+So if we want to hash a pair then we have to explicitly provide it with a hash function that can hash a pair.
+    unordered_map can takes upto 5 arguments: 
+ 
+
+Key : Type of key values
+Value : Type of value to be stored against the key
+Hash Function : A function which is used to hash the given key. If not provided it uses default hash function.
+Pred : A function which is used so that no two keys can have same hash values
+Alloc : An object used to define the memory model for the map
