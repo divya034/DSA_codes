97bdbed02cdb26e92506c05fa7125895f551cac5
divya034 <59786992+divya034@users.noreply.github.com>
2022-12-18 22:40:35 +0530

    Create Add Edges to Make Degrees of All Nodes Even.cpp

---
+++ qeetcode/Add Edges to Make Degrees of All Nodes Even.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isPossible(int n, vector<vector<int>>& edges) {
+        vector<unordered_set<int>> graph(n); 
+        for (auto& e : edges) {
+            graph[e[0]-1].insert(e[1]-1); 
+            graph[e[1]-1].insert(e[0]-1);
+        }
+        vector<int> nodes; 
+        for (int u = 0; u < n; ++u)
+            if (graph[u].size() & 1) nodes.push_back(u); 
+        if (nodes.size() == 0) return true; 
+        if (nodes.size() == 2) {
+            for (auto& g : graph) 
+                if (!g.count(nodes[0]) && !g.count(nodes[1])) return true; 
+            return false; 
+        }
+        if (nodes.size() == 4) {
+            int a = nodes[0], b = nodes[1], c = nodes[2], d = nodes[3]; 
+            if (!graph[a].count(b) && !graph[c].count(d) || !graph[a].count(c) && !graph[b].count(d) || !graph[a].count(d) && !graph[b].count(c)) return true; 
+            return false; 
+        }
+        return false; 
+    }
+};
