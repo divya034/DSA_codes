4d7a40540a3750a3f40812c39eafa67b29ab9e30
divya034 <59786992+divya034@users.noreply.github.com>
2022-12-31 14:11:57 +0530

    Create sqrtWithoutFunc.cpp

---
+++ sortingandsearching/sqrtWithoutFunc.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    int mySqrt(int x) {
+
+        int start = 0;
+        int end = x;
+       long long int mid = start + (end - start)/2;
+        long long int ans = -1;
+
+        while(start <= end){
+            long long int sq = mid * mid;
+
+            if(sq == x){
+                return mid;
+            }
+
+            if(sq < x){
+                ans = mid;
+                start = mid + 1;
+            }
+
+            else{
+                   end = mid - 1;
+            }
+         mid = start + (end - start)/2;
+            
+        }
+
+       return ans;
+    }
+};
