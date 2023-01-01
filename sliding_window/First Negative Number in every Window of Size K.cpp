b231d469cfa2d9c54ca9b74de4dc28382f64d784
divya034 <59786992+divya034@users.noreply.github.com>
2023-01-01 19:50:03 +0530

    Create First Negative Number in every Window of Size K.cpp

---
+++ sliding_window/First Negative Number in every Window of Size K.cpp
@@ -0,0 +1,26 @@
+vector<long long> printFirstNegativeInteger(long long int A[] , long long int N, long long int K) {
+    int start=0,end=0;
+    deque<long long> ans;
+    vector<long long> res;
+    while(end < N){
+        if(A[end] < 0){
+            ans.push_back(A[end]);
+        }
+        if(end-start+1 < K){
+            end++;
+        }
+        else if(end-start+1 == K){
+            if(ans.size()==0){
+                res.push_back(0);
+            }
+            else{
+                res.push_back(ans.front());
+                if(A[start]<0)
+                    ans.pop_front();
+            }
+            start++;
+            end++;
+        }
+    }
+    return res;
+}
