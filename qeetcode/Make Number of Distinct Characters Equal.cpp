1edbca57552958165dd4c1f878ab339b1780a065
divya034 <59786992+divya034@users.noreply.github.com>
2023-01-10 09:18:09 +0530

    Update Make Number of Distinct Characters Equal.cpp

--- qeetcode/Make Number of Distinct Characters Equal.cpp
+++ qeetcode/Make Number of Distinct Characters Equal.cpp
@@ -22,3 +22,28 @@ public:
         return false; 
     }
 };
+
+
+class Solution {
+public:
+    bool isItPossible(string word1, string word2) {
+        vector<int> f1(26), f2(26); 
+        for (auto& ch : word1) ++f1[ch-'a']; 
+        for (auto& ch : word2) ++f2[ch-'a']; 
+        for (auto& c1 : string("abcdefghijklmnopqrstuvwxyz")) 
+            for (auto& c2 : string("abcdefghijklmnopqrstuvwxyz")) 
+                if (f1[c1-'a'] && f2[c2-'a']) {
+                    --f1[c1-'a']; ++f1[c2-'a']; 
+                    --f2[c2-'a']; ++f2[c1-'a']; 
+                    int diff = 0; 
+                    for (int i = 0; i < 26; ++i) {
+                        if (f1[i]) ++diff; 
+                        if (f2[i]) --diff; 
+                    }
+                    if (diff == 0) return true; 
+                    ++f1[c1-'a']; --f1[c2-'a']; 
+                    ++f2[c2-'a']; --f2[c1-'a']; 
+                }
+        return false; 
+    }
+};
